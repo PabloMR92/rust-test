9da02e4d75df2f81875b10205a7c225474957961
Pablo Rodriguez <pablomr92@hotmail.com>
2020-07-19 10:47:55 -0300

    Fix X2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use rocket::{self, get, routes};
+#[macro_use] extern crate rocket;
 
-/// Declare a handler.
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-/// Start our server.
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
 }
